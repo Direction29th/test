5c871dec5b1f1b7229f5ca871fe46d340049a7b8
huzhan <huz.han@foxmail.com>
2023-12-08 11:30:55 +0800

    new master commit 2

--- Maven-Web/src/main/java/test.java
+++ Maven-Web/src/main/java/test.java
@@ -7,5 +7,7 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("master,master,master,master");
+        System.out.println("master,master,master,master");
+        System.out.println("master,master,master,master");
     }
 }
