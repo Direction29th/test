fef03300c7b01bc8d0ece5ff03cfe400cd65a535
Direction29th <52339687+Direction29th@users.noreply.github.com>
2023-12-08 11:21:56 +0800

    Update test.java

--- Maven-Web/src/main/java/test.java
+++ Maven-Web/src/main/java/test.java
@@ -6,6 +6,7 @@
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("master,master,master,master");
+        System.out.println("master,master,master,mast2131231231er");
+        System.out.println("master,master,master,master1232132132321312");
     }
 }
